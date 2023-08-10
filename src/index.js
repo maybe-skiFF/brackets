28f5f02671c8955a55cc45210ba25af4c162f99d
maybe-skiFF <den.skiff190@gmail.com>
2023-08-10 19:44:32 +0300

    feat: add solution brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closedBrackets = [];
+  let stack = [];
+  
+  bracketsConfig.forEach(el => {
+      openBrackets.push(el[0]);
+      closedBrackets.push(el[1]);
+  });
+
+  for (i = 0; i < str.length; i++) {
+      let curr = str[i];
+      let last = stack.slice(-1);
+      let isOpen = openBrackets.includes(curr);
+      let isClose = closedBrackets.includes(curr);
+
+      if (!isClose && !isOpen) {
+          continue;
+      }
+      if (isClose && isOpen) {
+        (last != curr) ? stack.push(curr) : stack.pop();
+          continue;
+      }
+      if (isOpen) {
+          stack.push(curr);
+      } else {
+          if (stack.length == 0) {
+              return false;
+          }
+          if (last == openBrackets[closedBrackets.indexOf(curr)]) {
+              stack.pop();
+          }
+      }
+  }
+  return stack.length === 0;
 }
